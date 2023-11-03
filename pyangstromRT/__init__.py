1d7da3b166336e3724867961f68ad04efc68a6cd
RuralBrick <48705917+RuralBrick@users.noreply.github.com>
2023-11-02 22:27:37 -0700

    Undate __init__.py.

--- pyangstromRT/__init__.py
+++ pyangstromRT/__init__.py
@@ -5,4 +5,4 @@ Date: Oct,19,2019
 Create this file to make Angstrom method great again!
 """
 
-from pyangstromRT.blmcmc import batch_mcmc_multi_chains
+from pyangstromRT.blmcmc import least_square_regression_Angstrom
