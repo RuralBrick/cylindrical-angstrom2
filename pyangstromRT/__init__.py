308220af7676527ad4e891e1611e4de59e234665
RuralBrick <48705917+RuralBrick@users.noreply.github.com>
2024-02-01 15:18:04 -0800

    Update __init__.py

--- pyangstromRT/__init__.py
+++ pyangstromRT/__init__.py
@@ -5,4 +5,4 @@ Date: Oct,19,2019
 Create this file to make Angstrom method great again!
 """
 
-from pyangstromRT.blmcmc import least_square_regression_Angstrom
+from pyangstromRT.blmcmc import calculate_theoretical_results, least_square_regression_Angstrom, multi_chain_Metropolis_Hasting
